daae47149f038a19a3633f5f2b90bd1806467b75
juaduan <juaduan@microsoft.com>
2022-09-06 14:53:11 +0800

    add homework 1

---
+++ homework1/sum.py
@@ -0,0 +1,13 @@
+"""
+Problem: Implement a function that takes two numbers and output their sum.
+"""
+
+
+def sum(x: float, y: float) -> float:
+    """
+    :param x: The first input.
+    :param y: The second input.
+    return: sum of x and y
+    """
+    # Your implementation goes here.
+    pass
